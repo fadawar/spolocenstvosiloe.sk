74bee98b65fb997b3e3d01f14ce0064870e26438
fadawar <fadawar@gmail.com>
2016-06-29 11:39:04 +0200

    Add Django project

--- start_develop.sh
+++ start_develop.sh
@@ -2,5 +2,5 @@
 
 vagrant up
 vagrant ssh -c "source /home/vagrant/venv/bin/activate;
-                cd /vagrant/src/main/;
+                cd /vagrant/src/siloe/;
                 python3 manage.py runserver 0.0.0.0:8000;"
