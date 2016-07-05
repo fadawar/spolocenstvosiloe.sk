0ebefef099045e5b57a35c0bae421301568fd219
fadawar <fadawar@gmail.com>
2016-07-05 23:31:11 +0200

    Add activating current page in menu

--- src/siloe/siloe/urls.py
+++ src/siloe/siloe/urls.py
@@ -23,12 +23,24 @@ from articles import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^about/$', TemplateView.as_view(template_name="static_pages/about.html")),
-    url(r'^service/map/$', TemplateView.as_view(template_name="static_pages/service-map.html")),
-    url(r'^service/worship-vision/$', TemplateView.as_view(template_name="static_pages/service-worship-vision.html")),
-    url(r'^service/dates/$', TemplateView.as_view(template_name="static_pages/service-dates.html")),
-    url(r'^support/billings/$', TemplateView.as_view(template_name="static_pages/support/billings.html")),
-    url(r'^support/sdm-2016/$', TemplateView.as_view(template_name="static_pages/support/sdm-2016.html")),
+    url(r'^about/$', TemplateView.as_view(
+        template_name="static_pages/about.html",
+        get_context_data=lambda: {'activated_menu_about': 'active'})),
+    url(r'^service/map/$', TemplateView.as_view(
+        template_name="static_pages/service-map.html",
+        get_context_data=lambda: {'activated_menu_map': 'active', 'activated_menu_service': 'active'})),
+    url(r'^service/worship-vision/$', TemplateView.as_view(
+        template_name="static_pages/service-worship-vision.html",
+        get_context_data=lambda: {'activated_menu_worship_vision': 'active', 'activated_menu_service': 'active'})),
+    url(r'^service/dates/$', TemplateView.as_view(
+        template_name="static_pages/service-dates.html",
+        get_context_data=lambda: {'activated_menu_dates': 'active', 'activated_menu_service': 'active'})),
+    url(r'^support/billings/$', TemplateView.as_view(
+        template_name="static_pages/support/billings.html",
+        get_context_data=lambda: {'activated_menu_billings': 'active', 'activated_menu_support': 'active'})),
+    url(r'^support/sdm-2016/$', TemplateView.as_view(
+        template_name="static_pages/support/sdm-2016.html",
+        get_context_data=lambda: {'activated_menu_sdm_2016': 'active', 'activated_menu_support': 'active'})),
     url(r'^articles/tags/(\d+)/$', views.view_articles_with_tag, name='view_articles_with_tag'),
     url(r'^articles/(\d+)/$', views.view_article, name='view_article'),
     url(r'^$', views.home_page, name='home'),
